b2ef8ff22e795321c0884e0e717ccedbf08f49e2
smart_dev_engineer <smart.dev.engineer@gmail.com>
2025-05-14 17:00:37 +0900

    add

--- scripts/account/U-49.sh
+++ scripts/account/U-49.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "U-49: 불필요한 계정 제거"
-EXTRA_USERS=$(awk -F: '$3 >= 1000 && $1 != "nobody" && $7 != "/sbin/nologin" && $7 != "/bin/false"' /etc/passwd)
+EXTRA_USERS=$(awk -F: '  $3 >= 1000 && $1 != "nobody" && $7 != "/sbin/nologin" && $7 != "/bin/false"' /etc/passwd)
 if [ -n "$EXTRA_USERS" ]; then
-    echo "경고: 다음 사용자 계정이 존재합니다:"
+    echo "  경고: 다음 사용자 계정이 존재합니다:"
     echo "$EXTRA_USERS"
     echo "  [취약] (불필요한 계정 존재 가능)"
 else
