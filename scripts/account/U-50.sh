1b7937f731a614ad8eb946b9f19064f66f8995c0
smart_dev_engineer <smart.dev.engineer@gmail.com>
2025-05-15 10:14:59 +0900

    add

--- scripts/account/U-50.sh
+++ scripts/account/U-50.sh
@@ -1,9 +1,9 @@
 echo "U-50: 관리자 그룹에 최소한의 사용자 등록"
 root_users=$(grep "^root" /etc/group | cut -d: -f4)
-echo "root 그룹 사용자: $root_users"
+echo "  root 그룹 사용자: $root_users"
 if [ "$root_users" = "root" ] || [ -z "$root_users" ]; then
-    echo "결과: 양호"
+    echo "  [양호]"
 else
-    echo "결과: 취약"
+    echo "  [취약]"
 fi
 echo
