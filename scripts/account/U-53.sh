672e860866da3373cd60685291fea77cacb4deca
smart_dev_engineer <smart.dev.engineer@gmail.com>
2025-05-15 10:13:15 +0900

    add

--- scripts/account/U-53.sh
+++ scripts/account/U-53.sh
@@ -1,8 +1,7 @@
 echo "U-53: 시스템 계정에 로그인 shell 부여 금지"
 issues=$(awk -F: '$3<1000 && $7 !~ /(nologin|false)/ {print $1}' /etc/passwd)
 if [ -n "$issues" ]; then
-    echo "  로그인 가능한 시스템 계정: $issues"
-    echo "  [취약]"
+    echo "  [취약] 로그인 가능한 시스템 계정: $issues"
 else
     echo "  [양호]"
 fi
