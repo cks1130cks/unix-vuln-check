3709cee939deb992b5efbb03b6e8305f47310da3
smart_dev_engineer <smart.dev.engineer@gmail.com>
2025-05-15 11:01:42 +0900

    add

--- scripts/account/U-51.sh
+++ scripts/account/U-51.sh
@@ -3,8 +3,15 @@
 echo "U-51: 불필요한 그룹 존재 여부 점검"
 
 # 허용 그룹 리스트 (필요에 따라 수정)
-ALLOWED_GROUPS=("root" "bin" "daemon" "sys" "adm" "wheel" "tty" "disk" "lp" "mail" "news" "uucp" "man" "proxy" "kmem" "dip" "www-data" "backup" "operator" "list" "irc" "gnats" "shadow" "utmp" "video" "games" "users" "nogroup")
-
+ALLOWED_GROUPS=(
+  "root" "bin" "daemon" "sys" "adm" "wheel" "tty" "disk" "lp" "mail" "news" "uucp"
+  "man" "proxy" "kmem" "dip" "www-data" "backup" "operator" "list" "irc" "gnats"
+  "shadow" "utmp" "video" "games" "users" "nogroup"
+  "mem" "cdrom" "dialout" "floppy" "tape" "ftp" "lock" "audio" "nobody" "utempter"
+  "avahi-autoipd" "ssh_keys" "input" "systemd-journal" "systemd-bus-proxy" "systemd-network"
+  "dbus" "polkitd" "tss" "postdrop" "postfix" "sshd" "wins" "rpc" "rpcuser" "nfsnobody"
+  "saslauth" "mailnull" "smmsp" "apache"
+)
 result="양호"
 while IFS=: read -r group_name _; do
     if [[ ! " ${ALLOWED_GROUPS[*]} " =~ " ${group_name} " ]]; then
