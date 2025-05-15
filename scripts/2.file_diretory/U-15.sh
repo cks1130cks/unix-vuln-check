0daf649411fefa09dfcb09b781a1f58e118b7818
smart_dev_engineer <smart.dev.engineer@gmail.com>
2025-05-15 14:20:22 +0900

    add

--- scripts/2.file_diretory/U-15.sh
+++ scripts/2.file_diretory/U-15.sh
@@ -1,10 +1,13 @@
 echo "U-15: 불필요한 world writable 파일 존재 여부 점검"
 
+echo "  점검 대상: 루트 파일 시스템(/) 내 스티키 비트가 설정되지 않은 world writable 일반 파일"
+
 output=$(find / -xdev -type f -perm -002 ! -perm -1000 2>/dev/null)
 
 if [ -z "$output" ]; then
     echo "  [양호] 불필요한 world writable 파일이 존재하지 않습니다."
 else
     echo "  [취약] 다음과 같은 불필요한 world writable 파일이 존재합니다:"
+    echo ""
     echo "$output"
 fi
