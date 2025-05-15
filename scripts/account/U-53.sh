440847f47a2ed7ecf85c6ca2dcfa84a37adc7f71
smart_dev_engineer <smart.dev.engineer@gmail.com>
2025-05-15 10:26:50 +0900

    add

--- scripts/account/U-53.sh
+++ scripts/account/U-53.sh
@@ -1,7 +1,8 @@
 echo "U-53: 시스템 계정에 로그인 shell 부여 금지"
 issues=$(awk -F: '$3<1000 && $7 !~ /(nologin|false)/ {print $1}' /etc/passwd)
 if [ -n "$issues" ]; then
-    echo "  [취약] 로그인 가능한 시스템 계정: $issues"
+    echo "  [취약] 로그인 가능한 시스템 계정:"
+    echo "$issues"
 else
     echo "  [양호]"
 fi
--- scripts/file_diretory/U-06.sh
+++ scripts/file_diretory/U-06.sh
@@ -6,7 +6,7 @@ orphan_files=$(find / \( -nouser -o -nogroup \) -print 2>/dev/null)
 
 if [ -n "$orphan_files" ]; then
     echo "  [취약] 소유자 없는 파일 존재:"
-    echo "  $orphan_files"
+    echo "$orphan_files"
 else
     echo "  [양호] 소유자 없는 파일 없음."
 fi
